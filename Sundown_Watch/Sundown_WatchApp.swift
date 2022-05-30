aeb78d96146914c6fd113d9f39422db3ade349ca
Airwreck <eeirikannu@gmail.com>
2022-05-29 21:31:30 -0500

    Initial Commit

---
+++ Sundown_Watch/Sundown_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sundown_WatchApp.swift
+//  Sundown_Watch
+//
+//  Created by AirwreckEye on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Sundown_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
